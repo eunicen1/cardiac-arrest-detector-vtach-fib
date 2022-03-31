fefff3e6573fc2269e87112126933ba2678ace08
Eunice Nwaobi <42991373+eunicen1@users.noreply.github.com>
2022-03-31 10:27:38 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #predicts 0 for normal + 1 for VFIB/VTACH
 data = # enter incoming signal in Volts (divide by mV if necessary)
-data = data[:,1] #emg 1
+data = data[:,1] #emg 1 if necessary otherwise comment out
 n = len(data)
 plt.plot(list(range(0, n)), data)
 plt.xlabel("Progression of time (s)")
